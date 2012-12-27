7e6cd02ff6303e0aa22daf876365f73688b0c477
Michał Koperkiewicz <kopringo@gmail.com>
2012-12-27 01:18:48 +0100

    first commit

---
+++ gramps_web_viewer/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'gramps_web_viewer.views.home', name='home'),
+    # url(r'^gramps_web_viewer/', include('gramps_web_viewer.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
